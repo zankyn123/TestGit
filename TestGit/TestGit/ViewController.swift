7d730044549c7c6132a1bded098290c0866f1016
Hung <hung@Manhs-Mac-mini.local>
2024-08-14 21:10:12 +0700

    add files

---
+++ TestGit/TestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Manh Hung on 14/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
